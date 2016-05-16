10927339d272e44bf344174397ed8d6c5333b3c8
Kevin Carbonell <famouskevinc@gmail.com>
2016-05-16 10:05:20 -0700

    Removed message. Created a header.

--- GitHubTestProject/GitHubTestProject/Form1.cs
+++ GitHubTestProject/GitHubTestProject/Form1.cs
@@ -19,7 +19,7 @@ namespace GitHubTestProject
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.Text = "Welcome";
+            this.Text = "Welcome!";
         }
     }
 }
