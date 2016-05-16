42ebaf37ae0737ef56b779005a05eab5aa9301ea
Kevin Carbonell <famouskevinc@gmail.com>
2016-05-16 09:53:05 -0700

    Added a greeting message

--- GitHubTestProject/GitHubTestProject/Form1.cs
+++ GitHubTestProject/GitHubTestProject/Form1.cs
@@ -16,5 +16,10 @@ namespace GitHubTestProject
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
     }
 }
